bca6525173addc8518649c558cdebe94d09895c8
Emily Dolson <emilyldolson@gmail.com>
2018-04-26 06:24:18 -0400

    Improved data tracking

--- source/map_elites_gp.h
+++ source/map_elites_gp.h
@@ -2,6 +2,7 @@
 #define __MAP_ELITES_GP_H__
 
 #include <set>
+#include <sys/stat.h>
 
 #include "Evolve/World.h"
 #include "hardware/AvidaGP.h"
@@ -16,6 +17,7 @@
 EMP_BUILD_CONFIG( MEGPConfig,
   GROUP(DEFAULT, "Default settings for box experiment"),
   VALUE(SEED, int, 55711224, "Random number seed (0 for based on time)"),
+  VALUE(TOURNAMENT_SIZE, uint32_t, 2, "Number of organisms in the popoulation."),
   VALUE(POP_SIZE, uint32_t, 100, "Number of organisms in the popoulation."),
   VALUE(UPDATES, uint32_t, 1000, "How many generations should we process?"),
   VALUE(SELECTION, std::string, "MAPELITES", "What selection scheme should we use?"),
@@ -40,14 +42,17 @@ public:
     long unsigned int ENTROPY_RES;
     double INST_MUT_RATE;
     double ARG_MUT_RATE;
+    uint32_t TOURNAMENT_SIZE;
     uint32_t POP_SIZE;
     uint32_t UPDATES;
     uint32_t N_TEST_CASES;    
     std::string SELECTION;
     std::string PROBLEM;
 
+    emp::DataNode<double, emp::data::Range> evolutionary_distinctiveness;
+
     TestcaseSet<int, double> testcases;
-    std::function<double(emp::AvidaGP&)> goal_function = [this](emp::AvidaGP org){
+    std::function<double(emp::AvidaGP&)> goal_function = [this](emp::AvidaGP & org){
         double score = 0;
         emp::Random rand = GetRandom();
         // for (int testcase : testcases.GetSubset(N_TEST_CASES, &rand)) {
@@ -73,6 +78,10 @@ public:
         return score;
     };
 
+    std::function<double(size_t id)> goal_function_ptr = [this](size_t id){
+        return CalcFitnessID(id);
+    };
+
     MapElitesGPWorld(){;}
     MapElitesGPWorld(emp::Random & rnd) : emp::World<emp::AvidaGP>(rnd) {;}
 
@@ -87,12 +96,24 @@ public:
         return scopes.size(); 
     };
 
+   std::function<int(size_t id)> scope_count_fun_ptr = [this](size_t id){ 
+        return scope_count_fun(*pop[id]);
+    };
+
     std::function<size_t(emp::AvidaGP::Instruction&)> GetInstID = [](emp::AvidaGP::Instruction & inst){return inst.id;};
 
     std::function<double(emp::AvidaGP &)> inst_ent_fun = [this](emp::AvidaGP & val){ 
         emp::vector<size_t> inst_list = emp::ApplyFunction(GetInstID, val.GetGenome().sequence);
         return emp::ShannonEntropy(inst_list); 
     };
+
+    std::function<double(size_t id)> inst_ent_fun_ptr = [this](size_t id){ 
+        return inst_ent_fun(*pop[id]);
+    };
+
+    std::function<emp::vector<size_t>() > get_pop = [this](){return GetValidOrgIDs();};
+
+    std::function<size_t(size_t)> return_id = [](size_t id){return id;};
 
     void Setup(MEGPConfig & config) {
         SetCache();
@@ -116,19 +137,52 @@ public:
         SetupFitnessFile().SetTimingRepeat(10);
         SetupSystematicsFile().SetTimingRepeat(10);
         SetupPopulationFile().SetTimingRepeat(10);
-        
+
+        emp::Ptr<emp::ContainerDataFile<emp::vector<size_t> > > tfile;
+        tfile.New("traits.dat");
+
+        auto & trait_file = static_cast<emp::ContainerDataFile<emp::vector<size_t> >& >(AddDataFile(tfile));
+        trait_file.SetUpdateContainerFun(get_pop);
+        trait_file.AddContainerFun(inst_ent_fun_ptr, "instruction_entropy", "Entropy of instructions");
+        trait_file.AddContainerFun(scope_count_fun_ptr, "scope_count", "Number of scopes used");
+        trait_file.AddContainerFun(goal_function_ptr, "fitness", "Fitness");
+        trait_file.AddContainerFun(return_id, "id", "ID");
+        trait_file.AddVar(update, "update", "Update");
+        trait_file.SetTimingRepeat(10);
+        trait_file.PrintHeaderKeys();
+
+        OnUpdate([this](int ud){if (ud % 100 == 0){SnapshotSingleFile(ud);}});
+
         testcases.LoadTestcases(PROBLEM);
         SetFitFun(goal_function);
         emp::AvidaGP org;
         AddPhenotype("Num Scopes", scope_count_fun, 1, 17);
         AddPhenotype("Entropy", inst_ent_fun, 0, -1*emp::Log2(1.0/org.GetInstLib()->GetSize())+.1);
-        emp::SetMapElites(*this, {SCOPE_RES, ENTROPY_RES});
+        if (SELECTION == "MAPELITES") {
+            emp::SetMapElites(*this, {SCOPE_RES, ENTROPY_RES});
+        }
 
         InitPop();
     }
 
+    void SnapshotSingleFile(size_t update) {
+        std::string snapshot_dir = "pop_" + emp::to_string((int)update);
+        mkdir(snapshot_dir.c_str(), ACCESSPERMS);
+        // For each program in the population, dump the full program description in a single file.
+        std::ofstream prog_ofstream(snapshot_dir + "/pop_" + emp::to_string((int)update) + ".pop");
+        for (size_t i : GetValidOrgIDs())
+        {
+            if (i)
+            prog_ofstream << "===\n";
+            prog_ofstream << "id: " << i << std::endl;
+            pop[i]->PrintGenome(prog_ofstream);
+        }
+        prog_ofstream.close();
+    }
+
     void InitConfigs(MEGPConfig & config) {
         SEED = config.SEED();
+        TOURNAMENT_SIZE = config.TOURNAMENT_SIZE();
         GENOME_SIZE = config.GENOME_SIZE();
         EVAL_TIME = config.EVAL_TIME();
         SCOPE_RES = config.SCOPE_RES();
@@ -144,7 +198,7 @@ public:
 
     void InitPop() {
         emp::Random & random = GetRandom();
-        for (int i = 0 ; i < 100; i++) {
+        for (int i = 0 ; i < POP_SIZE; i++) {
             emp::AvidaGP cpu;
             cpu.PushRandom(random, GENOME_SIZE);
             Inject(cpu.GetGenome());
@@ -152,8 +206,16 @@ public:
     }
 
     void RunStep() {
+        evolutionary_distinctiveness.Reset();
         std::cout << update << std::endl;
-        emp::RandomSelectSparse(*this, POP_SIZE);
+        if (SELECTION == "MAPELITES") {
+            emp::RandomSelectSparse(*this, POP_SIZE);
+        } else if (SELECTION =="TOURNAMENT") {
+            emp::TournamentSelect(*this, TOURNAMENT_SIZE, POP_SIZE);
+        } else {
+            emp_assert(false && "INVALID SELECTION SCEHME", SELECTION);
+        }
+
         Update();
     }
 
