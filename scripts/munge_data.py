858a50e4b90a864b536676049d353d24a8efef35
Emily Louise Dolson <emilyldolson@gmail.com>
2018-04-26 23:52:17 -0400

    Fix mung_data

--- scripts/munge_data.py
+++ scripts/munge_data.py
@@ -20,10 +20,11 @@ def main():
     frames = []
     trait_frames = []
 
-	# print(glob_pattern, glob.glob(glob_pattern))
-
+    # print(glob_pattern, glob.glob(glob_pattern))
+    print(glob.glob(glob_pattern))
     for dirname in glob.glob(glob_pattern):
         run_log = dirname + "/run.log"
+        print(dirname, run_log) 
         if not (os.path.exists(run_log)):
             print("run log not found")
             continue
@@ -32,7 +33,7 @@ def main():
 
         with open(run_log) as run_log_file:
             for line in run_log_file:
-                if line.startswith("Doing initial"):
+                if line.startswith("0"):
                     break
                 elif not line.startswith("set"):
                     continue
@@ -40,10 +41,6 @@ def main():
                 local_data[line[1]] = line[2]
 
 		# Create function
-
-        temp = pd.DataFrame()
-        for k in local_data:
-            temp[k] = local_data[k]
 
         fitness_df = pd.read_csv(dirname+"/fitness.csv", index_col="update")
         systematics_df = pd.read_csv(dirname+"/systematics.csv", index_col="update")
@@ -54,19 +51,17 @@ def main():
         # lin_df = pd.read_csv(dirname+"/lineage_mutations.csv", index_col="update")
 
         df = pd.concat([fitness_df, systematics_df, population_df], axis=1)
-        frames.append(pd.concat([df, temp]))
-        trait_frames.append(pd.concat[trait_df, temp])
+        for k in local_data:
+            df[k] = local_data[k]
+            trait_df[k] = local_data[k]
+
+        frames.append(df)
+        trait_frames.append(trait_df)
 
     all_data = pd.concat(frames)
     trait_data = pd.concat(trait_frames)
     all_data.to_csv(outfilename+".csv",index=False)
     trait_data.to_csv(outfilename+"_traits.csv", index=False)
-
-	#print(df)
-	# Evaluate :-)
-	# x = np.ones(2)
-	# value = f.evaluate(x)
-
 
 
 
